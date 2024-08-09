cffcd8cbbda81b170ce7e7ff397c07c214ccfa70
Matheus Altrão <matheusaltrao2@gmail.com>
2024-08-08 22:51:21 -0400

    fix: title name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Olímpiadas",
-  description: "Fique por dentro das novidades das olímpiadas ",
+  title: "Olimpíadas",
+  description: "Fique por dentro das novidades das limpíadas ",
   icons: {
     icon: [
       {
